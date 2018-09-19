45fcdc3f7a7649029f532d00857c5a64d113ea3a
Erin Green <erin.y.green@gmail.com>
2018-09-19 20:06:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,8 @@ end
 
 def printer(name_array)
  name_array.each do |name|
-   print batch_badge_creator(name)
-   print assign_rooms(name)
+   puts batch_badge_creator(name)
+   puts assign_rooms(name)
  end
 end
 
