0ac1e27b33d752782de51effff609c24448e5329
Erin Green <erin.y.green@gmail.com>
2018-09-19 19:24:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+ end
+ 
+def batch_badge_creator(name_array)
+  name_array.collect do |name|
+  badge_maker(name)
+  end
+end
+
+def assign_rooms(name_array)
+  name_array.each_with_index do |speaker, index|
+  return new_array = ["Hello, #{speaker}! You'll be assigned to room #{index + 1}!"]
+end 
+
+end
+
+def printer(name_array)
+  name_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ batch_badge_creator(name_array)
+ assign_rooms(name_array)
+end
