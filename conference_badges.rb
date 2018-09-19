7730880cc3f29c630a8c447fee7662e88367e12a
Erin Green <erin.y.green@gmail.com>
2018-09-19 19:42:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(name_array)
 end
 
 def assign_rooms(name_array)
- name_array.each_with_index do |speaker, index|
+ name_array.each_with_index.map do |speaker, index|
  "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
  end
 end 
